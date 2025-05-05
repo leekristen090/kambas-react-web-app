9999f80c1cd32a8af67016dbcc66d72037327309
Kristen Lee <kristenlee@MacBook-Air.local>
2025-05-05 18:00:43 -0400

    Paragraphs and Lists

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -2,7 +2,7 @@ import Lab1 from "./Lab1";
 
 export default function Labs() {
     return (
-        <div id={"wd-lab"}>
+        <div id={"wd-lab"} style={{textAlign: "left"}}>
             <h1>Labs</h1>
             <Lab1 />
         </div>
