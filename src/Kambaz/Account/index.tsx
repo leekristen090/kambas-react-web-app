b03e81d10187e8540923e7815721ddaba9a88dc5
Kristen Lee <kristenlee@MacBook-Air.local>
2025-05-07 20:54:28 -0400

    add nav

--- src/Kambaz/Account/index.tsx
+++ src/Kambaz/Account/index.tsx
@@ -2,17 +2,24 @@ import Signin from "./Signin.tsx";
 import Profile from "./Profile.tsx";
 import Signup from "./Signup.tsx";
 import {Navigate, Route, Routes} from "react-router-dom";
+import AccountNavigation from "./Navigation.tsx";
 
 export default function Account() {
     return (
         <div id={"wd-account-screen"}>
-            <h2>Account</h2>
-            <Routes>
-                <Route path={"/"} element={<Navigate to={"/Kambaz/Account/Signin"}/>} />
-                <Route path={"/Signin"} element={<Signin />} />
-                <Route path={"/Profile"} element={<Profile />} />
-                <Route path={"/Signup"} element={<Signup />} />
-            </Routes>
+            <table>
+                <tr>
+                    <td valign={"top"}> <AccountNavigation /> </td>
+                    <td valign={"top"}>
+                        <Routes>
+                            <Route path={"/"} element={<Navigate to={"/Kambaz/Account/Signin"}/>} />
+                            <Route path={"/Signin"} element={<Signin />} />
+                            <Route path={"/Profile"} element={<Profile />} />
+                            <Route path={"/Signup"} element={<Signup />} />
+                        </Routes>
+                    </td>
+                </tr>
+            </table>
         </div>
     );
 }
