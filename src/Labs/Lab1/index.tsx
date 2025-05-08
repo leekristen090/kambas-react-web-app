d2a5736c239a6c6c690d8184ec83d4619c49e4a5
Kristen Lee <kristenlee@MacBook-Air.local>
2025-05-07 22:18:03 -0400

    add courses

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -2,7 +2,6 @@ export default function Lab1() {
     return (
         <div id="wd-lab1">
             <h2>Lab 1</h2>
-            <h2>Debug for branch a1</h2>
             <h3>HTML Examples</h3>
             <div id={"wd-h-tag"}>
                 <h4>Heading Tags</h4>
