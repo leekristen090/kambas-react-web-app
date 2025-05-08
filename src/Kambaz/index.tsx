3034e56290279013f36593006c6c047ff114834d
Kristen Lee <kristenlee@MacBook-Air.local>
2025-05-07 21:47:35 -0400

    add kam nav

--- src/Kambaz/index.tsx
+++ src/Kambaz/index.tsx
@@ -1,16 +1,23 @@
 import {Routes, Route, Navigate} from "react-router-dom";
 import Account from "./Account";
-import Dashboard from "./Dashboard.tsx";
+import Dashboard from "./Dashboard";
+import KambazNavigation from "./Navigation.tsx";
 
 export default function Kambaz() {
     return(
         <div id={"wd-kambaz"}>
-            <h1>Kambaz</h1>
-            <Routes>
-                <Route path={"/"} element={<Navigate to={"Account"}/> } />
-                <Route path={"/Account/*"} element={<Account />} />
-                <Route path={"/Dashboard"} element={<Dashboard />} />
-            </Routes>
+            <table>
+                <tr>
+                    <td valign={"top"}><KambazNavigation/></td>
+                    <td valign={"top"}>
+                        <Routes>
+                            <Route path={"/"} element={<Navigate to={"Account"}/> } />
+                            <Route path={"/Account/*"} element={<Account />} />
+                            <Route path={"/Dashboard"} element={<Dashboard />} />
+                        </Routes>
+                    </td>
+                </tr>
+            </table>
         </div>
     );
 }
