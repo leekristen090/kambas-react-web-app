c7311e357fa84ed07e0ccc3256e520cce57000b1
Kristen Lee <kristenlee@MacBook-Air.local>
2025-05-07 21:19:27 -0400

    add dashboard

--- src/Kambaz/index.tsx
+++ src/Kambaz/index.tsx
@@ -1,5 +1,6 @@
 import {Routes, Route, Navigate} from "react-router-dom";
 import Account from "./Account";
+import Dashboard from "./Dashboard.tsx";
 
 export default function Kambaz() {
     return(
@@ -8,6 +9,7 @@ export default function Kambaz() {
             <Routes>
                 <Route path={"/"} element={<Navigate to={"Account"}/> } />
                 <Route path={"/Account/*"} element={<Account />} />
+                <Route path={"/Dashboard"} element={<Dashboard />} />
             </Routes>
         </div>
     );
