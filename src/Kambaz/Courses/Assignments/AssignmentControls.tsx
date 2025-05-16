5cb0f9bfea34c960a3bc0e7d5c5d23c5cd5e0821
Kristen Lee <kristenlee@MacBook-Air.local>
2025-05-15 20:11:00 -0400

    edits to assignment modules

--- src/Kambaz/Courses/Assignments/AssignmentControls.tsx
+++ src/Kambaz/Courses/Assignments/AssignmentControls.tsx
@@ -1,20 +1,22 @@
-import {Button, Form} from "react-bootstrap";
+import {Button, FormControl, InputGroup} from "react-bootstrap";
 import {FaPlus} from "react-icons/fa6";
+import {CiSearch} from "react-icons/ci";
 
 export default function AssignmentControls() {
     return(
         <div id={"wd-assignment-controls"} className={"text-nowrap"}>
-            <Button variant={"danger"} size="lg" className="me-1 float-end" id="wd-add-assignment-btn">
-                <FaPlus className="position-relative me-2" style={{ bottom: "1px" }} />
+            <InputGroup className={"w-25 float-start"} id={"wd-assignment-search-box"}>
+                <InputGroup.Text><CiSearch /></InputGroup.Text>
+                <FormControl placeholder={"Search..."} />
+            </InputGroup>
+            <Button variant={"danger"} size={"lg"} className={"me-1 float-end"} id={"wd-add-assignment-btn"}>
+                <FaPlus className={"position-relative me-2"} style={{ bottom: "1px" }} />
                 Assignment
             </Button>
-            <Button variant={"secondary"} size="lg" className="me-1 float-end" id="wd-add-group-btn">
-                <FaPlus className="position-relative me-2" style={{ bottom: "1px" }} />
+            <Button variant={"secondary"} size={"lg"} className={"me-1 float-end"} id={"wd-add-group-btn"}>
+                <FaPlus className={"position-relative me-2"} style={{ bottom: "1px" }} />
                 Group
             </Button>
-            <Form className="me-1 float-end">
-                <Form.Control type={"textarea"} placeholder={"Search.."} size={"lg"}/>
-            </Form>
         </div>
     );
 }
