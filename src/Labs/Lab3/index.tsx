a5b6f18861aa4797befe3b95744c6ac071a2ee5a
Kristen Lee <kristenlee@MacBook-Air.local>
2025-05-20 20:16:55 -0400

    ch 3.1-3.3

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,7 +1,30 @@
+import VariablesAndConstants from "./VariablesAndConstants.tsx";
+import VariableTypes from "./VariableTypes.tsx";
+import BooleanVariables from "./BooleanVariables.tsx";
+import IfElse from "./IfElse.tsx";
+import TernaryOperator from "./TernaryOperator.tsx";
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse.tsx";
+import ConditionalOutputInline from "./ConditionalOutputInline.tsx";
+import LegacyFunctions from "./LegacyFunctions.tsx";
+import ArrowFunctions from "./ArrowFunctions.tsx";
+import ImpliedReturn from "./ImpliedReturn.tsx";
+import TemplateLiterals from "./TemplateLiterals.tsx";
+
 export default function Lab3() {
     return (
         <div id={"wd-lab3"}>
             <h2>Lab 3</h2>
+            <VariablesAndConstants />
+            <VariableTypes />
+            <BooleanVariables />
+            <IfElse />
+            <TernaryOperator />
+            <ConditionalOutputIfElse />
+            <ConditionalOutputInline />
+            <LegacyFunctions />
+            <ArrowFunctions />
+            <ImpliedReturn />
+            <TemplateLiterals />
         </div>
     );
 }
