1105c6ff169b624d57f1be9c0924a218bc6ce14f
Kristen Lee <kristenlee@MacBook-Air.local>
2025-05-23 18:49:55 -0400

    data driven assignments

--- src/Kambaz/Courses/Assignments/index.tsx
+++ src/Kambaz/Courses/Assignments/index.tsx
@@ -4,72 +4,36 @@ import {BsGripVertical} from "react-icons/bs";
 import AssignmentControlButtons from "./AssignmentControlButtons.tsx";
 import { MdAssignment } from "react-icons/md";
 import LessonControlButtons from "../Modules/LessonControlButtons.tsx";
+import {Link, useParams} from "react-router";
+import * as db from "../../Database";
 
 export default function Assignments() {
+    const { cid } = useParams();
+    const assignments = db.assignments;
     return (
         <div id={"wd-assignments"}>
             <AssignmentControls /><br /><br /><br /><br />
             <ListGroup className="rounded-0" id="wd-assignments">
-                <ListGroup.Item className="wd-module p-0 mb-5 fs-5 border-gray">
+                <ListGroup.Item className={"wd-module p-0 mb-5 fs-5 border-gray"}>
                     <div className="wd-title p-3 ps-2 bg-secondary">
                         <BsGripVertical className={"me-2 fs-3"} /> ASSIGNMENTS <AssignmentControlButtons />
                     </div>
-                    <ListGroup className={"wd-assignment rounded-0"}>
-                        <ListGroup.Item className={"wd-assign p-3 ps-1"}>
-                            <div className={"d-flex align-items-center"}>
-                                <BsGripVertical className={"me-2 fs-3"}/><MdAssignment className={"text-success"}/>
-                                <div className={"flex-grow-1 wd-margin-right-left"}>
-                                    <a href={"#/Kambaz/Courses/1234/Assignments/123"}
-                                       className={"wd-assignment-link"}>
-                                        A1 - ENV + HMTL
-                                    </a>
-                                    <div className={"wd-assignment-details text-muted small mt-1"}>
-                                        <span>
-                                            <span className={"text-danger"}>Multiple Modules </span>
-                                            <span><b>| Not Available until</b> May 6 at 12:00am | <b>Due</b> May 13 at 11:59pm | 100 pts</span>
-                                        </span>
+                    {assignments.filter((assignment: any) => assignment.course === cid).map((assignment: any) =>(
+                        <ListGroup className={"wd-assignment rounded-0"}>
+                            <ListGroup.Item className={"wd-assign p-3 ps-1"}>
+                                <div className={"d-flex align-items-center"}>
+                                    <BsGripVertical className={"me-2 fs-3"}/><MdAssignment className={"text-success"}/>
+                                    <div className={"flex-grow-1 wd-margin-right-left"}>
+                                        <a className={"wd-assignment-link"}
+                                           href={`#/Kambaz/Courses/${cid}/Assignments/${assignment._id}`}>
+                                            {assignment.title}
+                                        </a>
                                     </div>
+                                    <LessonControlButtons />
                                 </div>
-                                <LessonControlButtons />
-                            </div>
-                        </ListGroup.Item>
-                        <ListGroup.Item className={"wd-assign p-3 ps-1"}>
-                            <div className={"d-flex align-items-center"}>
-                                <BsGripVertical className={"me-2 fs-3"}/><MdAssignment className={"text-success"}/>
-                                <div className={"flex-grow-1 wd-margin-right-left"}>
-                                    <a href={"#/Kambaz/Courses/1234/Assignments/123"}
-                                       className={"wd-assignment-link"}>
-                                        A2 - CSS + BOOTSTRAP
-                                    </a>
-                                    <div className={"wd-assignment-details text-muted small mt-1"}>
-                                        <span>
-                                            <span className={"text-danger"}>Multiple Modules </span>
-                                            <span><b>| Not Available until</b> May 13 at 12:00am | <b>Due</b> May 20 at 11:59pm | 100 pts</span>
-                                        </span>
-                                    </div>
-                                </div>
-                                <LessonControlButtons />
-                            </div>
-                        </ListGroup.Item>
-                        <ListGroup.Item className={"wd-assign p-3 ps-1"}>
-                            <div className={"d-flex align-items-center"}>
-                                <BsGripVertical className={"me-2 fs-3"}/><MdAssignment className={"text-success"}/>
-                                <div className={"flex-grow-1 wd-margin-right-left"}>
-                                    <a href={"#/Kambaz/Courses/1234/Assignments/123"}
-                                       className={"wd-assignment-link"}>
-                                        A3 - JAVASCRIPT + REACT
-                                    </a>
-                                    <div className={"wd-assignment-details text-muted small mt-1"}>
-                                        <span>
-                                            <span className={"text-danger"}>Multiple Modules </span>
-                                            <span><b>| Not Available until</b> May 20 at 12:00am | <b>Due</b> May 27 at 11:59pm | 100 pts</span>
-                                        </span>
-                                    </div>
-                                </div>
-                                <LessonControlButtons />
-                            </div>
-                        </ListGroup.Item>
-                    </ListGroup>
+                            </ListGroup.Item>
+                        </ListGroup>
+                    ))}
                 </ListGroup.Item>
             </ListGroup>
         </div>
