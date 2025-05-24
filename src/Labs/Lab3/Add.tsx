9f83a836b1cb65655de6e986ad8acc8a2e787004
Kristen Lee <kristenlee@MacBook-Air.local>
2025-05-24 12:07:49 -0400

    edits

--- src/Labs/Lab3/Add.tsx
+++ src/Labs/Lab3/Add.tsx
@@ -2,7 +2,7 @@ export default function Add({ a, b }: { a: number; b: number }) {
     return (
         <div id={"wd-add"}>
             <h4>Add</h4>
-            a = {a}
+            a = {a} <br/>
             b = {b}<br />
             a + b = {a + b}<hr />
         </div>
