575aeb907ea180d03392ca65c766873b2f86e413
Kristen Lee <kristenlee@MacBook-Air.local>
2025-05-27 11:38:57 -0400

    add lab 4

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -3,10 +3,11 @@ import { Route, Routes, Navigate } from "react-router";
 import TOC from "./TOC";
 import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
+import Lab4 from "./Lab4";
 
 export default function Labs() {
     return (
-        <div style={{textAlign: "left"}}>
+        <div>
             <h1>Labs</h1>
             <h2>Kristen Lee CS 5610 Summer 1</h2>
             <TOC/>
@@ -15,6 +16,7 @@ export default function Labs() {
                 <Route path={"Lab1"} element={<Lab1 />} />
                 <Route path={"Lab2/*"} element={<Lab2 />} />
                 <Route path={"Lab3/*"} element={<Lab3 />} />
+                <Route path={"Lab4/*"} element={<Lab4 />} />
             </Routes>
         </div>
     );
