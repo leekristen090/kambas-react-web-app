19e565a1e58adacd94b16ab994274c433de5eb74
Kristen Lee <kristenlee@MacBook-Air.local>
2025-05-27 11:53:33 -0400

    starting a4

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -1,7 +1,10 @@
+import ClickEvent from "./ClickEvent.tsx";
+
 export default function Lab4() {
     return (
-        <div id={"wd-lab4"}>
+        <div id={"wd-lab4"} className={"container"}>
             <h2>Lab 4</h2>
+            <ClickEvent />
         </div>
     );
 }
