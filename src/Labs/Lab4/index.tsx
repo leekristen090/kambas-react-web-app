5de38a9e088ad29313892c11a919f344d3f59201
Kristen Lee <kristenlee@MacBook-Air.local>
2025-05-27 12:33:03 -0400

    ch 4.2.1

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -1,10 +1,19 @@
 import ClickEvent from "./ClickEvent.tsx";
+import PassingDataOnEvent from "./PassingDataOnEvent.tsx";
+import PassingFunctions from "./PassingFunctions.tsx";
+import EventObject from "./EventObject.tsx";
 
 export default function Lab4() {
+    function sayHello() {
+        alert("Hello");
+    }
     return (
         <div id={"wd-lab4"} className={"container"}>
             <h2>Lab 4</h2>
             <ClickEvent />
+            <PassingDataOnEvent />
+            <PassingFunctions theFunction={sayHello} />
+            <EventObject />
         </div>
     );
 }
